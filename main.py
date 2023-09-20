9bd262657a3cc09064f13191cbdb07a4295b9b86
liyunlooong <145371317+liyunlooong@users.noreply.github.com>
2023-09-20 15:35:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ async def root():
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Awesome cloud developer dff9 says hello {name}"}
+    return {
+        "message": f"Hello {name}",
+        "Sender": "Yunlong Li"
+    }
 
 
 @app.get("/hello_text/{name}")
